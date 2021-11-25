9e9359b9f82d7c728dc1389c6d6ad291e2f38fba
shayan1718 <69643787+shayan1718@users.noreply.github.com>
2021-11-25 10:55:47 +0330

    Update mac-changer2.py

--- mac-changer2.py
+++ mac-changer2.py
@@ -4,7 +4,7 @@ import optparse
 def get_argument():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="interface for change")
-    parser.add_option("-m", "new_mac", dest="new_mac", help="new mac addres you want")
+    parser.add_option("-m", "--new_mac", dest="new_mac", help="new mac addres you want")
     (options, arguments) = parser.parse_args()
     if not options.interface:
         parser.error("please specify an interface, use --help for more help")
